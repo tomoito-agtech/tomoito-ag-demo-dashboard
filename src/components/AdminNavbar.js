b822792a1ebc075572f9276a18109ae975cb0710
tomoito <tomoito@agtech.co.jp>
2021-11-11 16:45:22 +0900

    commit

--- src/components/AdminNavbar.js
+++ src/components/AdminNavbar.js
@@ -1,11 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
-import NavbarInput from '@material-tailwind/react/NavbarInput';
-import Image from '@material-tailwind/react/Image';
-import Dropdown from '@material-tailwind/react/Dropdown';
-import DropdownItem from '@material-tailwind/react/DropdownItem';
-import ProfilePicture from 'assets/img/team-1-800x800.jpg';
+
 
 export default function AdminNavbar({ showSidebar, setShowSidebar }) {
     const location = useLocation().pathname;
